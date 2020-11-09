3e94352d7f9ad2284b336416e7816017fbec01cd
raloliver <contato@raloliver.com.br>
2020-11-09 05:42:21 +0000

    signup page finished

--- src/app/auth/store/effects/signup.effect.ts
+++ src/app/auth/store/effects/signup.effect.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 
 import {createEffect, Actions, ofType} from '@ngrx/effects';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import {catchError, map, switchMap, tap} from 'rxjs/operators';
 import {of} from 'rxjs';
 
 import {
@@ -12,26 +13,44 @@ import {
 import {AuthService} from '@app/auth/services/auth.service';
 import {CurrentUserInterface} from '@shared/types/currentUser.interface';
 import {HttpErrorResponse} from '@angular/common/http';
+import {PersistanceService} from '@shared/services/persistance.service';
+import {dispatch} from 'rxjs/internal/observable/pairs';
 
 @Injectable()
 export class SignupEffect {
-  constructor(private actions$: Actions, private authService: AuthService) {}
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private persistanceService: PersistanceService,
+    private router: Router
+  ) {}
 
   signup$ = createEffect(() =>
     this.actions$.pipe(
       ofType(signupAction),
       switchMap(({request}) => {
-        return this.authService
-          .signup(request)
-          .pipe(
-            map((currentUser: CurrentUserInterface) =>
-              signupSuccessAction({currentUser})
-            )
-          );
+        return this.authService.signup(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            // avoit this inside of effect window.localStorage.setItem('AUTH_TOKEN', currentUser.token);
+            this.persistanceService.set('AUTH_TOKEN', currentUser.token);
+            return signupSuccessAction({currentUser});
+          })
+        );
       }),
       catchError((errorResponse: HttpErrorResponse) =>
         of(signupErrorAction({errors: errorResponse.error.errors}))
       )
     )
+  );
+
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(signupSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/');
+        })
+      ),
+    {dispatch: false}
   );
 }
---
+++ src/app/shared/modules/tab-feed/tab-feed/components/tab-feed/tab-feed.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'nb-tab-feed',
+  templateUrl: './tab-feed.component.html',
+  styleUrls: ['./tab-feed.component.scss'],
+})
+export class TabFeedComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
