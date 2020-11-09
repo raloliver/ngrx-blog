49ed7e143b7505b1d8161664b0471511b8914cc2
raloliver <contato@raloliver.com.br>
2020-11-09 04:39:03 +0000

    update auth reducers

--- src/app/auth/store/actions/signup.action.ts
+++ src/app/auth/store/actions/signup.action.ts
@@ -3,13 +3,17 @@ import {createAction, props} from '@ngrx/store';
 import {ActionTypes} from '@app/auth/store/actionTypes';
 import {SignupRequestInterface} from '@app/auth/types/signupRequest.interface';
 import {CurrentUserInterface} from '@shared/types/currentUser.interface';
+import {ApiErrosInterface} from '@shared/types/apiErros.interface';
 
 export const signupAction = createAction(
   ActionTypes.SIGNUP,
   props<{request: SignupRequestInterface}>()
 );
 
-export const signupErrorAction = createAction(ActionTypes.SIGNUP_ERROR);
+export const signupErrorAction = createAction(
+  ActionTypes.SIGNUP_ERROR,
+  props<{errors: ApiErrosInterface}>()
+);
 
 export const signupSuccessAction = createAction(
   ActionTypes.SIGNUP_SUCCESS,
--- src/app/auth/types/authState.interface.ts
+++ src/app/auth/types/authState.interface.ts
@@ -1,3 +1,9 @@
+import {ApiErrosInterface} from '@shared/types/apiErros.interface';
+import {CurrentUserInterface} from '@shared/types/currentUser.interface';
+
 export interface AuthStateInterface {
   isSent: boolean;
+  currentUser: CurrentUserInterface | null;
+  isLogged: boolean | null;
+  validationError: ApiErrosInterface | null;
 }
